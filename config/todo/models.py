6ca91b1dd6562d71c53bf7427f445499189813bd
y.mori <a.m.h.yu90@gmail.com>
2022-01-22 19:47:04 +0900

    モデルの追加等の処理を加えた

--- config/todo/models.py
+++ config/todo/models.py
@@ -1,10 +1,54 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoCategory(models.Model):
+    name = models.CharField(
+        '業務種別',
+        max_length=255,
+        blank=True,
+        null=True,
+        unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TodoStatus(models.Model):
+    name = models.CharField(
+        '処理状況',
+        max_length=255,
+        blank=True,
+        null=True,
+        unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Todo(models.Model):
-    title = models.CharField("タスク名", max_length=30)
-    description = models.TextField("詳細", blank=True)
-    deadline = models.DateField("締切")
+    title = models.CharField(
+        "タスク名", 
+        max_length=30, 
+        help_text='【社名】を記載してください')
+
+    description = models.TextField(
+        "詳細",
+        blank=True,
+        help_text='作成物の対象期間や属性などの詳細を記入してください')
+
+    deadline = models.DateField(
+        "締切")
+    
+    category = models.ForeignKey(
+        TodoCategory,
+        on_delete=models.CASCADE,
+        verbose_name="業務種別")
+    
+    status = models.ForeignKey(
+        TodoStatus,
+        on_delete=models.CASCADE,
+        verbose_name="処理状況")
+    
 
     def __str__(self):
         return self.title
--- config/todo/views.py
+++ config/todo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from .models import Todo
 
@@ -26,12 +28,23 @@ class TodoDetail(DetailView):
         return context
 
 class TodoCreate(CreateView):
+    # template_name = 'todo/todo_form.html'
     model = Todo
     # fieldeで入力する項目を設定する。今回は「title」「description」「deadline」の全てを入力したいため「__all__」で全選択している
+    # 1つずつ選択する場合の書き方は「fields = ['title', 'description', 'deadline']」。models.pyの定義通り。
     fields = "__all__"
     # タスクが作成できたらreverse_lazy()でlistという名前のページに遷移させる
     # listは「todo_list.html」のことみたいだ
-    success_url = reverse_lazy("list")
+    # success_url = reverse_lazy("list")
+
+    # detailに遷移させる場合は、個別ページのurlが必要だから以下のようにする
+    def get_success_url(self):
+        return reverse_lazy("detail", kwargs={'pk':self.object.pk})
+    
+    # csrf_tokenを無効化したいときは以下のコードを記載する
+    # @method_decorator(csrf_exempt)
+    # def dispatch(self, *args, **kwargs):
+    #     return super(TodoCreate, self).dispatch(*args, **kwargs)
 
 class TodoUpdate(UpdateView):
     model = Todo
